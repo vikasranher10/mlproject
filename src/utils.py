6034fcd5e0f7f3ff0d949226730f5e6b46d8db9b
Vikas Ranher <vikasranher10@gmail.com>
2024-08-03 11:46:43 +0530

    data transformation

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+
+from src.exception import CustomException
+ 
+def save_object(file_path, obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.mkdir(dir_path, exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj, file_obj)
+            
+    except Exception as e:
+        raise CustomException(e, sys)
